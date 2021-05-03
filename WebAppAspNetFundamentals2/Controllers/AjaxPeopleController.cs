ee961b24ba9102c0dd25e59d057397aa241caf1e
tanto untung <untung2k3@yahoo.com>
2021-05-04 01:35:49 +0200

    need consultation, please

--- WebAppAspNetFundamentals2/Controllers/AjaxPeopleController.cs
+++ WebAppAspNetFundamentals2/Controllers/AjaxPeopleController.cs
@@ -13,11 +13,49 @@ namespace WebAppAspNetFundamentals2.Controllers
     {
         IPeopleService _peopleService = new PeopleService();
 
-        [HttpGet]
         public IActionResult AjaxIndex()
         {
             return View();
         }
+        
+        
+        [HttpGet]
+        public IActionResult AjaxShowAll()
+        {
+            return PartialView("_PeoplePatialView", _peopleService.All());
+        }
+
+        [HttpPost]
+        public IActionResult DetailPerson(int id)
+        {
+            Person person = _peopleService.FindBy(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView("_PersonPatialView", person);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Person person = _peopleService.FindBy(id);
+
+            if (person == null)
+            {
+                return NotFound();//404
+            }
+
+            if (_peopleService.Remove(id))
+            {
+                return Ok("person" + id);//200
+            }
+
+            return BadRequest();
+        }
+
     }
 }
 
--- WebAppAspNetFundamentals2/wwwroot/js/site.js
+++ WebAppAspNetFundamentals2/wwwroot/js/site.js
@@ -1,10 +1,19 @@
-﻿
-function All(event) {
+﻿document.getElementById("showAllPeople").
+    addEventListener("click", ShowAll);
+
+document.getElementById("showDetail").
+    addEventListener("click", DetailPerson);
+
+document.getElementById("deletePerson").
+    addEventListener("click", DeletePerson);
+
+
+function ShowAll(event) {
     event.preventDefault();
     console.log("OnSubmit Event: ", event);
 
     var formUrl = event.target.action;
-    console.log("formUrl", formUrl);
+    console.log("formUrl: ", formUrl);
 
     $.get(formUrl, function (data, status) {
         console.log("Data: " + data + "\nStatus: " + status);
@@ -12,4 +21,56 @@ function All(event) {
         $("#PeopleTable").replaceWith(data);
     });
 }
+
+function DetailPerson(event, id) {
+    event.preventDefault();
+    console.log("Event: ", event);
+
+    console.log("id: ", document.getElementById("keyId"));
+
+    var anchorUrl = event.target.href;
+    console.log("anchorUrl: ", anchorUrl);
+
+    $post(anchorUrl,
+        {
+            id: document.getElementById("keyId")
+        },
+        function (data, status) {
+            console.log("Data: " + data + "\nStatus: " + status);
+
+            $("#PeopleTable" + id).replaceWith(data);
+        }
+    );
+}
+
+function DeletePerson(event, id) {
+    event.preventDefault();
+    console.log("Event: ", event);
+
+    console.log("id: ", document.getElementById("keyId"));
+
+    var deleteUrl = event.target.href;
+    console.log("deleteUrl: ", deleteUrl);
+
+    $.post(deleteUrl,
+        {
+            id: document.getElementById("keyId")
+        },
+        function (data, status) {
+            alert("Data: " + data + "\nStatus: " + status);
+            $("#" + data).remove();
+        }
+    ).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("jqXHR", jqXHR);
+        console.log("textStatus", textStatus);
+        console.log("errorThrown", errorThrown);
+        if (jqXHR.status == 404) {
+            alert("Person not found.\nwas not able to delete.")
+        }
+        else {
+            alert("Status: " + jqXHR.status);
+        }
+    });
+}
+
 
