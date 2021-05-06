a0c44d4a1db73d90ed8db57f6fa694fbe0e5d7bc
tanto untung <untung2k3@yahoo.com>
2021-05-06 10:47:47 +0200

    assignment 5

---
+++ WebAppAspNetFundamentals2/Models/Repo/DatabasePeopleRepo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppAspNetFundamentals2.Database;
+using WebAppAspNetFundamentals2.Models.Data;
+
+namespace WebAppAspNetFundamentals2.Models.Repo
+{
+    public class DatabasePeopleRepo : IPeopleRepo
+    {
+        private readonly PeopleDbContext peopleDbContext;
+        public DatabasePeopleRepo(PeopleDbContext peopleDbContext)
+        {
+            this.peopleDbContext = peopleDbContext;
+        }
+
+        public Person Create(Person person)
+        {
+            peopleDbContext.People.Add(person);
+
+            int result = peopleDbContext.SaveChanges();
+
+            if (result == 0)
+            {
+                throw new Exception("unable to add person in database.");
+            }
+
+            return person;
+        }
+
+
+        public Person Read(int id)
+        {
+            return peopleDbContext.People.SingleOrDefault(row => row.Id == id);
+        }
+
+        public List<Person> Read()
+        {
+            return peopleDbContext.People.ToList();
+        }
+
+        public Person Update(Person person)
+        {
+            Person originalPerson = Read(person.Id);
+
+            if (originalPerson == null)
+            {
+                return null;
+            }
+
+            originalPerson.Name = person.Name;
+            originalPerson.PhoneNumber = person.PhoneNumber;
+            originalPerson.City = person.City;
+
+            return originalPerson;
+        }
+        public bool Delete(Person person)
+        {
+            Person genuinePerson = Read(person.Id);
+
+            if (genuinePerson == null)
+            {
+                return false;
+            }
+
+            peopleDbContext.People.Remove(genuinePerson);
+
+            int result = peopleDbContext.SaveChanges();
+
+            if (result == 0)
+            {
+                throw new Exception("unable to delete person in database.");
+            }
+
+            return true;
+        }
+    }
+}
--- WebAppAspNetFundamentals2/Startup.cs
+++ WebAppAspNetFundamentals2/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,7 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAppAspNetFundamentals2.Database;
 using WebAppAspNetFundamentals2.Models;
+using WebAppAspNetFundamentals2.Models.Repo;
 using WebAppAspNetFundamentals2.Models.Service;
 
 namespace WebAppAspNetFundamentals2
@@ -25,8 +28,11 @@ namespace WebAppAspNetFundamentals2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PeopleDbContext>(options => options.
+            UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddScoped<IPeopleService, PeopleService>();
-            services.AddSingleton<IPeopleRepo, InMemoryPeopleRepo>();
+            services.AddScoped<IPeopleRepo, DatabasePeopleRepo>();
 
             services.AddMvc();
         }
