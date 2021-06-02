ba518599df7620bdfc014c8147123b333274c2be
tanto untung <untung2k3@yahoo.com>
2021-06-02 17:10:37 +0200

    menit 13

---
+++ WebAppAspNetFundamentals2/Controllers/ReactController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppAspNetFundamentals2.Models.Data;
+using WebAppAspNetFundamentals2.Models.Service;
+
+namespace WebAppAspNetFundamentals2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReactController : ControllerBase
+    {
+        private readonly IPeopleService _peopleService;
+
+        public ReactController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
+        [HttpGet]
+        public List<Person> Get()
+        {
+            return _peopleService.All().PeopleList;
+        }
+
+    }
+}
